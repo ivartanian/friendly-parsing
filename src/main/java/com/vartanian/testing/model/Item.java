b9072618df156b4b3068592690c69f3933c0badd
igor.vartanian <igor.vartanian@gmail.com>
2015-09-21 17:58:58 +0300

    OK

---
+++ src/main/java/com/vartanian/testing/model/Item.java
@@ -0,0 +1,41 @@
+package com.vartanian.testing.model;
+
+/**
+ * Created by super on 9/21/15.
+ */
+public class Item {
+
+    private String url;
+    private String score;
+    private String pass;
+
+    public Item(String url, String score, String pass) {
+        this.url = url;
+        this.score = score;
+        this.pass = pass;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
